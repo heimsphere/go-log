bf944a1698e52096213303c858f8823632ddfcfe
Ruben Jenster <rjenster@gmail.com>
2015-10-27 09:15:08 +0100

    Added static functions. Fixed vararg paramenters.

--- logger.go
+++ logger.go
@@ -12,10 +12,6 @@ type Logger struct {
 
 var Log *Logger
 
-func init() {
-	Log = Stdout()
-}
-
 func (l *Logger) EnableDebug() {
 	l.debug = true
 	l.Println("Debug logging enabled.")
@@ -45,16 +41,16 @@ func (l *Logger) Debugln(v ...interface{}) {
 
 // FIXME add ERROR prefix
 func (l *Logger) Error(v ...interface{}) {
-	l.Print(v...)
+	l.Print(v)
 }
 
 func (l *Logger) Errorf(format string, v ...interface{}) {
-	l.Printf("ERROR: "+format, v...)
+	l.Printf("ERROR: "+format, v)
 }
 
 // FIXME add ERROR prefix
 func (l *Logger) Errorln(v ...interface{}) {
-	l.Println(v...)
+	l.Println(v)
 }
 
 func Stdout() *Logger {
@@ -74,4 +70,44 @@ func (w *null) Write(p []byte) (n int, err error) {
 func (l *Logger) Disable() {
 	l.Logger = log.New(&null{}, "", log.LstdFlags)
 	l.debug = false
+}
+
+func init() {
+	Log = Stderr()
+}
+
+func Debugln(v ...interface{}) {
+	Log.Debugln(v)
+}
+
+func Debug(v ...interface{}) {
+	Log.Debug(v)
+}
+
+func Debugf(fmt string, v ...interface{}) {
+	Log.Debugf(fmt, v)
+}
+
+func Println(v ...interface{}) {
+	Log.Println(v)
+}
+
+func Print(v ...interface{}) {
+	Log.Print(v)
+}
+
+func Printf(fmt string, v ...interface{}) {
+	Log.Printf(fmt, v)
+}
+
+func Errorln(v ...interface{}) {
+	Log.Errorln(v)
+}
+
+func Error(v ...interface{}) {
+	Log.Error(v)
+}
+
+func Errorf(fmt string, v ...interface{}) {
+	Log.Errorf(fmt, v)
 }
